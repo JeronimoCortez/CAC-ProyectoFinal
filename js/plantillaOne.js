cdd7ffad59f0a26fe9c8484f1678eb712018d1f5
JeronimoCortez <jeronimofcortez3@gmail.com>
2024-05-27 09:35:25 -0300

    Actualizaciones 27/05/2024

--- js/plantillaOne.js
+++ js/plantillaOne.js
@@ -1,4 +1,7 @@
+//Esta script contiene unicamente la animacion de la seccion proyectos, puedes modificarlo como guste!
+
 function showProjectsDetails(container) {
     let projectDetails = container.querySelector(".project-click");
     projectDetails.classList.toggle("active");
 }
+
