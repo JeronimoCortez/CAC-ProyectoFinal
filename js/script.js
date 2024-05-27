44aaba8aea4b24244eee71d0b5acf79f87d86f46
JeronimoCortez <jeronimofcortez3@gmail.com>
2024-05-27 19:35:37 -0300

    Mobile menu

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// MENU MOBILE
+const menuToggle = document.getElementById('mobile-menu');
+const mobileNav = document.getElementById('mobile-nav');
+const closeBtn = document.getElementById('close-btn');
+
+menuToggle.addEventListener('click', () => {
+    mobileNav.classList.toggle('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    mobileNav.classList.remove('active');
+});
