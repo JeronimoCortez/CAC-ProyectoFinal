e0e0ba5c5549c4ef13a667d6e6319f9c683a5d8b
JeronimoCortez <jeronimofcortez3@gmail.com>
2024-05-28 18:10:29 -0300

    Mobile menu

--- js/plantillaOne.js
+++ js/plantillaOne.js
@@ -1,7 +1,18 @@
-//Esta script contiene unicamente la animacion de la seccion proyectos, puedes modificarlo como guste!
-
+// SECCION PROYECTOS
 function showProjectsDetails(container) {
     let projectDetails = container.querySelector(".project-click");
     projectDetails.classList.toggle("active");
 }
 
+// MENU MOBILE
+const menuToggle = document.getElementById('mobile-menu');
+const mobileNav = document.getElementById('mobile-nav');
+const closeBtn = document.getElementById('close-btn');
+
+menuToggle.addEventListener('click', () => {
+    mobileNav.classList.toggle('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    mobileNav.classList.remove('active');
+});
